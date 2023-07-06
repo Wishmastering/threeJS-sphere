a7c00abe0b90b288c1e3f0c2a47deb63b4e52401
Wishmastering <charsdb@gmail.com>
2023-07-06 16:37:58 -0600

    Final Tutorial Stage

--- main.js
+++ main.js
@@ -77,5 +77,23 @@ tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
 
 // Mouse Animation Colorrrr
 let mouseDown = false;
+let rgb = [12, 23, 55];
 window.addEventListener("mousedown", () => (mouseDown = true));
 window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.width) * 255),
+      150,
+    ];
+    // Let's animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
